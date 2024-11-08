0ec8c02628842db922c99ea1fc6363621f00883d
ejshafran <ejshafran@hilma.tech>
2024-11-08 17:07:06 +0200

    ✨ Initial URL request loading

---
+++ browser.py
@@ -0,0 +1,66 @@
+import socket
+
+
+class URL:
+    def __init__(self, url: str):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http"
+        if not "/" in url:
+            url = url + "/"
+
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+    def request(self):
+        # Open socket connection
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP
+        )
+        s.connect((self.host, 80))
+        # Send request (as bytes)
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+        # Read response into file-like object
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        # Read version, status, and explanation
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+        # Read headers
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            # Headers are case insensitive
+            response_headers[header.casefold()] = value.strip()
+        # Ensure no tricky headers have been sent down
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        content = response.read()
+        return content
+
+
+def show(body: str):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+def load(url: URL):
+    body = url.request()
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
